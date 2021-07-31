1fd39b2f95061f0421158f78741114d77f4efb9d
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2021-07-31 15:16:40 -0400

    Latest

--- base_validator.py
+++ base_validator.py
@@ -18,7 +18,7 @@ class BaseValidator(Validator):
 
     def error_str(self):
         """Prettier format to a list of errors."""
-        return '\t' + '\n\t'.join((field + ': ' + pformat(error) for field, error in self.errors.items()))
+        return '\t' + '\n'.join((field + ': ' + pformat(error) for field, error in self.errors.items()))
 
     def _isdir(self, field, value):
         """Validate value is a valid, existing directory."""
